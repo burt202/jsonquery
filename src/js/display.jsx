ee04dd241b91514d538f9cbe922f024cf3f13f2e
Aaron Burtnyk <aaron.burtnyk@momentumft.co.uk>
2016-07-25 14:10:24 +0100

    Move js and css into separate folders

---
+++ src/js/display.jsx
@@ -0,0 +1,125 @@
+var React = require("react");
+var R = require("ramda");
+
+var formatter = require("./formatter");
+var Filters = require("./filters");
+
+var Display = React.createClass({
+  displayName: "Display",
+
+  propTypes: {
+    actionCreator: React.PropTypes.object.isRequired,
+    filters: React.PropTypes.array.isRequired,
+    groupBy: React.PropTypes.string,
+    schema: React.PropTypes.object.isRequired,
+    data: React.PropTypes.array.isRequired
+  },
+
+  onAddFilter: function (e) {
+    this.props.actionCreator.addFilter(e.target.value);
+  },
+
+  onGroupByChange: function (e) {
+    this.props.actionCreator.groupBy(e.target.value);
+  },
+
+  onReset: function () {
+    this.props.actionCreator.reset();
+  },
+
+  getFilterOptions: function () {
+    var filterOptions = R.pipe(
+      R.keys,
+      R.without(R.pluck("name", this.props.filters))
+    )(this.props.schema);
+
+    return filterOptions.map(function (value) {
+      return (
+        <option value={value} key={value}>{value}</option>
+      );
+    });
+  },
+
+  getFilterControl: function () {
+    return (
+      <div className="input-control">
+        <span>Add Filter:</span>
+        <select onChange={this.onAddFilter}>
+          <option></option>
+          {this.getFilterOptions()}
+        </select>
+      </div>
+    );
+  },
+
+  getGroupByOptions: function () {
+    return Object.keys(this.props.schema).map(function (value) {
+      return (
+        <option value={value} key={value}>{value}</option>
+      );
+    });
+  },
+
+  getGroupByControl: function () {
+    return (
+      <div className="input-control">
+        <span>Group By:</span>
+        <select onChange={this.onGroupByChange} value={this.props.groupBy || ""}>
+          <option></option>
+          {this.getGroupByOptions()}
+        </select>
+      </div>
+    );
+  },
+
+  getResetControl: function () {
+    return (
+      <p><a href="#" onClick={this.onReset}>Reset</a></p>
+    );
+  },
+
+  showSummary: function (filtered, grouped) {
+    var formattedGroups = "None";
+
+    if (this.props.groupBy) {
+      var groups = Object.keys(grouped).map(function (key) {
+        return key + " (" + grouped[key].length + ")";
+      });
+
+      formattedGroups = groups.join(", ");
+    }
+
+    return (
+      <div>
+        <h3>Summary</h3>
+        <p>Total: {filtered.length}</p>
+        <p>Groups: {formattedGroups}</p>
+      </div>
+    );
+  },
+
+  render: function () {
+    var filtered = formatter.filter(this.props.data, this.props.schema, this.props.filters);
+    var grouped = formatter.group(filtered, this.props.groupBy);
+
+    return (
+      <div>
+        {this.getFilterControl()}
+        <Filters
+          actionCreator={this.props.actionCreator}
+          filters={this.props.filters}
+          schema={this.props.schema}
+        />
+        {this.getGroupByControl()}
+        {this.getResetControl()}
+        {this.showSummary(filtered, grouped)}
+        <div>
+          <h3>Results</h3>
+          <pre>{JSON.stringify(grouped, null, 2)}</pre>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Display;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var R = require("ramda");
 
-var actionCreator = require("./action-creator");
-var store = require("./store");
+var actionCreator = require("./js/action-creator");
+var store = require("./js/store");
+var Upload = require("./js/upload");
+var Display = require("./js/display");
 
-var formatter = require("./formatter");
-var Upload = require("./upload");
-var Filters = require("./filters");
-
-require("./app.css");
+require("./css/app.css");
 
 var Main = React.createClass({
   getInitialState: function () {
@@ -28,115 +25,19 @@ var Main = React.createClass({
     this.setState(store.getState());
   },
 
-  onAddFilter: function (e) {
-    actionCreator.addFilter(e.target.value);
-  },
-
-  onGroupByChange: function (e) {
-    actionCreator.groupBy(e.target.value);
-  },
-
-  onReset: function () {
-    actionCreator.reset();
-  },
-
-  getFilterOptions: function () {
-    var filterOptions = R.pipe(
-      R.keys,
-      R.without(R.pluck("name", this.state.filters))
-    )(this.state.schema);
-
-    return filterOptions.map(function (value) {
-      return (
-        <option value={value} key={value}>{value}</option>
-      );
-    });
-  },
-
-  getFilterControl: function () {
-    return (
-      <div className="input-control">
-        <span>Add Filter:</span>
-        <select onChange={this.onAddFilter}>
-          <option></option>
-          {this.getFilterOptions()}
-        </select>
-      </div>
-    );
-  },
-
-  getGroupByOptions: function () {
-    return Object.keys(this.state.schema).map(function (value) {
-      return (
-        <option value={value} key={value}>{value}</option>
-      );
-    });
-  },
-
-  getGroupByControl: function () {
-    return (
-      <div className="input-control">
-        <span>Group By:</span>
-        <select onChange={this.onGroupByChange} value={this.state.groupBy || ""}>
-          <option></option>
-          {this.getGroupByOptions()}
-        </select>
-      </div>
-    );
-  },
-
-  getResetControl: function () {
-    return (
-      <p><a href="#" onClick={this.onReset}>Reset</a></p>
-    );
-  },
-
-  showSummary: function (filtered, grouped) {
-    var formattedGroups = "None";
-
-    if (this.state.groupBy) {
-      var groups = Object.keys(grouped).map(function (key) {
-        return key + " (" + grouped[key].length + ")";
-      });
-
-      formattedGroups = groups.join(", ");
-    }
-
-    return (
-      <div>
-        <h3>Summary</h3>
-        <p>Total: {filtered.length}</p>
-        <p>Groups: {formattedGroups}</p>
-      </div>
-    );
-  },
-
   render: function () {
     if (!this.state.schema || !this.state.data) {
       return <Upload
         actionCreator={actionCreator}
       />
     } else {
-      var filtered = formatter.filter(this.state.data, this.state.schema, this.state.filters);
-      var grouped = formatter.group(filtered, this.state.groupBy);
-
-      return (
-        <div>
-          {this.getFilterControl()}
-          <Filters
-            actionCreator={actionCreator}
-            filters={this.state.filters}
-            schema={this.state.schema}
-          />
-          {this.getGroupByControl()}
-          {this.getResetControl()}
-          {this.showSummary(filtered, grouped)}
-          <div>
-            <h3>Results</h3>
-            <pre>{JSON.stringify(grouped, null, 2)}</pre>
-          </div>
-        </div>
-      )
+      return <Display
+        actionCreator={actionCreator}
+        filters={this.state.filters}
+        groupBy={this.state.groupBy}
+        schema={this.state.schema}
+        data={this.state.data}
+      />
     }
   }
 });
