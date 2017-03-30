6c9e8e3e2d4d13fa44ef1584be71861e3046018c
Aaron Burtnyk <aaron.burtnyk@momentumft.co.uk>
2017-03-30 13:38:10 +0100

    Split out filter row into its own component

---
+++ src/js/display/filter-row.jsx
@@ -0,0 +1,137 @@
+const React = require("react")
+const R = require("ramda")
+const classNames = require("classnames")
+
+const defaultInput = {}
+const dateInput = {placeholder: "YYYYMMDD", maxLength: 8}
+const dateTimeInput = {placeholder: "YYYYMMDD (hhmm)", maxLength: 13}
+const separateWithCommaInput = {placeholder: "separate with comma"}
+const regexInput = {placeholder: "enter regex here"}
+
+const filterConfig = {
+  string: [
+    {text: "Equal to", value: "eq", inputs: [defaultInput]},
+    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
+    {text: "Is null", value: "nl"},
+    {text: "Is not null", value: "nnl"},
+    {text: "Is one of", value: "iof", inputs: [separateWithCommaInput]},
+    {text: "Is not one of", value: "inof", inputs: [separateWithCommaInput]},
+    {text: "Matches", value: "rgm", inputs: [regexInput]},
+  ],
+  int: [
+    {text: "Equal to", value: "eq", inputs: [defaultInput]},
+    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
+    {text: "Is null", value: "nl"},
+    {text: "Is not null", value: "nnl"},
+    {text: "Greater than", value: "gt", inputs: [defaultInput]},
+    {text: "Greater than or equal to", value: "gte", inputs: [defaultInput]},
+    {text: "Less than", value: "lt", inputs: [defaultInput]},
+    {text: "Less than or equal to", value: "lte", inputs: [defaultInput]},
+    {text: "Is one of", value: "iof", inputs: [separateWithCommaInput]},
+    {text: "Is not one of", value: "inof", inputs: [separateWithCommaInput]},
+    {text: "Is between", value: "btw", inputs: [defaultInput, defaultInput]},
+  ],
+  bool: [
+    {text: "Is null", value: "nl"},
+    {text: "Is not null", value: "nnl"},
+    {text: "Is true", value: "true"},
+    {text: "Is false", value: "false"},
+  ],
+  date: [
+    {text: "Equal to", value: "eq", inputs: [defaultInput]},
+    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
+    {text: "Is same day as", value: "sd", inputs: [dateInput]},
+    {text: "Is before", value: "be", inputs: [dateTimeInput]},
+    {text: "Is after", value: "af", inputs: [dateTimeInput]},
+    {text: "Is between", value: "btw", inputs: [dateTimeInput, dateTimeInput]},
+    {text: "Is null", value: "nl"},
+    {text: "Is not null", value: "nnl"},
+  ],
+  array: [
+    {text: "Contains String", value: "cos", inputs: [defaultInput]},
+    {text: "Contains Number", value: "con", inputs: [defaultInput]},
+    {text: "Has length of", value: "hl", inputs: [defaultInput]},
+    {text: "Doesnt Have length of", value: "dhl", inputs: [defaultInput]},
+    {text: "Has length greater than", value: "hlgt", inputs: [defaultInput]},
+    {text: "Has length greater than or equal to", value: "hlgte", inputs: [defaultInput]},
+    {text: "Has length less than", value: "hllt", inputs: [defaultInput]},
+    {text: "Has length less than or equal to", value: "hllte", inputs: [defaultInput]},
+  ],
+}
+
+const FilterRow = React.createClass({
+  displayName: "FilterRow",
+
+  propTypes: {
+    actionCreator: React.PropTypes.object.isRequired,
+    filter: React.PropTypes.object.isRequired,
+    type: React.PropTypes.string.isRequired,
+  },
+
+  getInputControlByType: function(type, filter) {
+    if (!filterConfig[type]) return "Invalid Type"
+
+    const options = filterConfig[type].map(function(option) {
+      return <option key={option.value} value={option.value}>{option.text}</option>
+    })
+
+    var inputs = []
+    const selectedOperator = R.find(R.propEq("value", filter.operator), filterConfig[type])
+
+    if (selectedOperator && selectedOperator.inputs) {
+      inputs = selectedOperator.inputs.map(function(inputConfig, index) {
+        const inc = (index) ? index : ""
+
+        return React.createElement("input", R.merge({
+          key: filter.id + inc,
+          type: "text",
+          name: filter.id + inc,
+          value: filter["value" + inc] || "",
+          onChange: this.updateFilter.bind(this, "value" + inc),
+        }, inputConfig))
+      }.bind(this))
+    }
+
+    const classnames = classNames("filter-controls", type)
+
+    return (
+      <div className={classnames}>
+        <select name={filter.id} value={filter.operator} onChange={this.updateFilter.bind(this, "operator")}>
+          {options}
+        </select>
+        <div className="inputs">
+          {inputs}
+        </div>
+      </div>
+    )
+  },
+
+  toggleFilter: function() {
+    this.props.actionCreator.toggleFilter(this.props.filter.id, !this.props.filter.active)
+  },
+
+  deleteFilter: function() {
+    this.props.actionCreator.deleteFilter(this.props.filter.id)
+  },
+
+  updateFilter: function(prop, e) {
+    const toUpdate = {}
+    toUpdate[prop] = e.target.value
+    this.props.actionCreator.updateFilter(this.props.filter.id, toUpdate)
+  },
+
+  render: function() {
+    const toggleClass = (this.props.filter.active) ? "active" : "inactive"
+
+    return (
+      <tr key={this.props.filter.id} className={toggleClass}>
+        <td>{this.props.filter.name}</td>
+        <td>{this.getInputControlByType(this.props.type, this.props.filter)}</td>
+        <td><a className="site-link" onClick={this.toggleFilter}>Toggle</a></td>
+        <td><a className="site-link" onClick={this.deleteFilter}>Remove</a></td>
+      </tr>
+    )
+  },
+})
+
+module.exports = FilterRow
--- src/js/display/filters.jsx
+++ src/js/display/filters.jsx
@@ -1,63 +1,5 @@
 const React = require("react")
-const R = require("ramda")
-var classNames = require("classnames")
-
-const defaultInput = {}
-const dateInput = {placeholder: "YYYYMMDD", maxLength: 8}
-const dateTimeInput = {placeholder: "YYYYMMDD (hhmm)", maxLength: 13}
-const separateWithCommaInput = {placeholder: "separate with comma"}
-const regexInput = {placeholder: "enter regex here"}
-
-const filterConfig = {
-  string: [
-    {text: "Equal to", value: "eq", inputs: [defaultInput]},
-    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
-    {text: "Is null", value: "nl"},
-    {text: "Is not null", value: "nnl"},
-    {text: "Is one of", value: "iof", inputs: [separateWithCommaInput]},
-    {text: "Is not one of", value: "inof", inputs: [separateWithCommaInput]},
-    {text: "Matches", value: "rgm", inputs: [regexInput]},
-  ],
-  int: [
-    {text: "Equal to", value: "eq", inputs: [defaultInput]},
-    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
-    {text: "Is null", value: "nl"},
-    {text: "Is not null", value: "nnl"},
-    {text: "Greater than", value: "gt", inputs: [defaultInput]},
-    {text: "Greater than or equal to", value: "gte", inputs: [defaultInput]},
-    {text: "Less than", value: "lt", inputs: [defaultInput]},
-    {text: "Less than or equal to", value: "lte", inputs: [defaultInput]},
-    {text: "Is one of", value: "iof", inputs: [separateWithCommaInput]},
-    {text: "Is not one of", value: "inof", inputs: [separateWithCommaInput]},
-    {text: "Is between", value: "btw", inputs: [defaultInput, defaultInput]},
-  ],
-  bool: [
-    {text: "Is null", value: "nl"},
-    {text: "Is not null", value: "nnl"},
-    {text: "Is true", value: "true"},
-    {text: "Is false", value: "false"},
-  ],
-  date: [
-    {text: "Equal to", value: "eq", inputs: [defaultInput]},
-    {text: "Not equal to", value: "neq", inputs: [defaultInput]},
-    {text: "Is same day as", value: "sd", inputs: [dateInput]},
-    {text: "Is before", value: "be", inputs: [dateTimeInput]},
-    {text: "Is after", value: "af", inputs: [dateTimeInput]},
-    {text: "Is between", value: "btw", inputs: [dateTimeInput, dateTimeInput]},
-    {text: "Is null", value: "nl"},
-    {text: "Is not null", value: "nnl"},
-  ],
-  array: [
-    {text: "Contains String", value: "cos", inputs: [defaultInput]},
-    {text: "Contains Number", value: "con", inputs: [defaultInput]},
-    {text: "Has length of", value: "hl", inputs: [defaultInput]},
-    {text: "Doesnt Have length of", value: "dhl", inputs: [defaultInput]},
-    {text: "Has length greater than", value: "hlgt", inputs: [defaultInput]},
-    {text: "Has length greater than or equal to", value: "hlgte", inputs: [defaultInput]},
-    {text: "Has length less than", value: "hllt", inputs: [defaultInput]},
-    {text: "Has length less than or equal to", value: "hllte", inputs: [defaultInput]},
-  ],
-}
+const FilterRow = require("./filter-row")
 
 const Filters = React.createClass({
   displayName: "Filters",
@@ -68,44 +10,6 @@ const Filters = React.createClass({
     schema: React.PropTypes.object.isRequired,
   },
 
-  getInputControlByType: function(type, filter) {
-    if (!filterConfig[type]) return "Invalid Type"
-
-    const options = filterConfig[type].map(function(option) {
-      return <option key={option.value} value={option.value}>{option.text}</option>
-    })
-
-    var inputs = []
-    const selectedOperator = R.find(R.propEq("value", filter.operator), filterConfig[type])
-
-    if (selectedOperator && selectedOperator.inputs) {
-      inputs = selectedOperator.inputs.map(function(inputConfig, index) {
-        const inc = (index) ? index : ""
-
-        return React.createElement("input", R.merge({
-          key: filter.id + inc,
-          type: "text",
-          name: filter.id + inc,
-          value: filter["value" + inc] || "",
-          onChange: this.updateFilter.bind(this, filter.id, "value" + inc),
-        }, inputConfig))
-      }.bind(this))
-    }
-
-    const classnames = classNames("filter-controls", type)
-
-    return (
-      <div className={classnames}>
-        <select name={filter.id} value={filter.operator} onChange={this.updateFilter.bind(this, filter.id, "operator")}>
-          {options}
-        </select>
-        <div className="inputs">
-          {inputs}
-        </div>
-      </div>
-    )
-  },
-
   getFilterRows: function() {
     if (!this.props.filters.length)
       return (
@@ -113,39 +17,19 @@ const Filters = React.createClass({
       )
 
     return this.props.filters.map(function(filter) {
-      const toggleClass = (filter.active) ? "active" : "inactive"
-
-      return (
-        <tr key={filter.id} className={toggleClass}>
-          <td>{filter.name}</td>
-          <td>{this.getInputControlByType(this.props.schema[filter.name], filter)}</td>
-          <td><a className="site-link" onClick={this.toggleFilter} data-id={filter.id} data-active={filter.active}>Toggle</a></td>
-          <td><a className="site-link" onClick={this.deleteFilter} data-id={filter.id}>Remove</a></td>
-        </tr>
-      )
+      return <FilterRow
+        key={filter.id}
+        filter={filter}
+        type={this.props.schema[filter.name]}
+        actionCreator={this.props.actionCreator}
+      />
     }.bind(this))
-  },
-
-  toggleFilter: function(e) {
-    const id = e.target.dataset.id
-    const active = e.target.dataset.active === "true"
-    this.props.actionCreator.toggleFilter(id, !active)
-  },
-
-  deleteFilter: function(e) {
-    this.props.actionCreator.deleteFilter(e.target.dataset.id)
-  },
-
-  updateFilter: function(id, prop, e) {
-    const toUpdate = {}
-    toUpdate[prop] = e.target.value
-    this.props.actionCreator.updateFilter(id, toUpdate)
   },
 
   render: function() {
     return (
       <div>
-      <h3>Filters and Grouping</h3>
+        <h3>Filters and Grouping</h3>
         <table className="table filters">
           <thead>
             <tr>
