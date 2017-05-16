96d1ed9b943a17258ae392ec3b4f55cf8f8a9a47
Aaron Burtnyk <aaron.burtnyk@momentumft.co.uk>
2017-05-16 12:31:43 +0100

    Add drag/drop support for upload

--- src/js/home/upload.jsx
+++ src/js/home/upload.jsx
@@ -5,7 +5,44 @@ const Upload = React.createClass({
 
   propTypes: {
     onAction: React.PropTypes.func.isRequired,
-    errorDate: React.PropTypes.string,
+    errorDate: React.PropTypes.number,
+  },
+
+  getInitialState: function() {
+    return {
+      isDragActive: false,
+    }
+  },
+
+  onDragOver: function(e) {
+    e.preventDefault()
+
+    this.setState({
+      isDragActive: true,
+    })
+  },
+
+  onDragEnter: function(e) {
+    e.preventDefault()
+  },
+
+
+  onDragLeave: function() {
+    this.setState({
+      isDragActive: false,
+    })
+  },
+
+  onDrop: function(e) {
+    e.preventDefault()
+
+    this.setState({
+      isDragActive: false,
+    })
+
+    const reader = new FileReader()
+    reader.onload = this.onFileUploadEnd
+    reader.readAsText(e.dataTransfer.files[0])
   },
 
   onFileUploadStart: function(e) {
@@ -19,9 +56,23 @@ const Upload = React.createClass({
   },
 
   render: function() {
+    const style = {
+      borderColor: this.state.isDragActive ? "#000" : "#AAA",
+    }
+
     return (
       <div>
         <p><input type="file" key={this.props.errorDate} onChange={this.onFileUploadStart} /></p>
+        <div
+          style={style}
+          className="drag-drop-area"
+          onDragLeave={this.onDragLeave}
+          onDragOver={this.onDragOver}
+          onDragEnter={this.onDragEnter}
+          onDrop={this.onDrop}
+        >
+          <h3>Drop file here</h3>
+        </div>
       </div>
     )
   },
