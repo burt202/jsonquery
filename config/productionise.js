c1b3cfbfb806b634ba33e022adca912e57da32b3
Aaron Burtnyk <burt202@gmail.com>
2017-05-18 21:39:24 +0100

    Add css optimiser for prod build

--- config/productionise.js
+++ config/productionise.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack")
 const SwigWebpackPlugin = require("swig-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 const packageJson = require("../package.json")
 
@@ -8,7 +9,7 @@ const R = require("ramda")
 
 const prodPlugins = [
   new ExtractTextPlugin("bundle.css"),
-
+  new OptimizeCssAssetsPlugin(),
   new webpack.DefinePlugin({
     "process.env": {
       "NODE_ENV": JSON.stringify("production"),
@@ -46,9 +47,7 @@ module.exports = function(webpackConfig) {
   )
 
   const oldRules = webpackConfig.module.rules.slice(0, webpackConfig.module.rules.length - 1)
-
   const entry = webpackConfig.entry.slice(-1)
-
   const result = R.merge(webpackConfig, {plugins, entry})
 
   result.module.rules = oldRules.concat(prodRules)
