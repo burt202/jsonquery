5cb411236f07b2e40e2e3c87b099105668539f05
Aaron Burtnyk <burt202@gmail.com>
2017-07-02 18:36:12 +0100

    Use more chart options

--- src/js/query/results-display-chart.jsx
+++ src/js/query/results-display-chart.jsx
@@ -5,19 +5,67 @@ const R = require("ramda")
 
 const Bar = require("react-chartjs-2").Bar
 
-function ChartDisplay(props) {
-  const chartData = {
-    labels: R.pluck("name", props.data),
-    datasets: [{data: R.pluck("count", props.data)}],
-  }
-
-  return (
-    <Bar data={chartData} />
-  )
+const chartTypeMap = {
+  bar: Bar,
 }
 
-ChartDisplay.propTypes = {
-  data: PropTypes.any.isRequired,
-}
+const ChartDisplay = React.createClass({
+  displayName: "ChartDisplay",
+
+  propTypes: {
+    data: PropTypes.any.isRequired,
+  },
+
+  getInitialState() {
+    return {
+      type: "bar",
+      title: "",
+    }
+  },
+
+  onTitleChange(e) {
+    this.setState({
+      title: e.target.value,
+    })
+  },
+
+  render() {
+    const chartData = {
+      labels: R.pluck("name", this.props.data),
+      datasets: [{data: R.pluck("count", this.props.data)}],
+    }
+
+    const options = {
+      title: {
+        display: !!this.state.title.length,
+        text: this.state.title,
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          gridLines: {
+            display: false,
+          },
+        }],
+        xAxes: [{
+          gridLines: {
+            display: false,
+          },
+        }],
+      },
+    }
+
+    const Component = chartTypeMap[this.state.type]
+
+    return (
+      <div>
+        <p><input value={this.state.title} onChange={this.onTitleChange} placeholder="Chart name here..." /></p>
+        <Component data={chartData} options={options} />
+      </div>
+    )
+  },
+})
 
 module.exports = ChartDisplay
